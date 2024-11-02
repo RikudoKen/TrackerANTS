902cf84ff45ae98cd283c7cebd6e5bcbd844fe94
RikudoKen <141680797+RikudoKen@users.noreply.github.com>
2024-11-01 23:10:08 -0600

    Lab 3
    Props complete

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );
